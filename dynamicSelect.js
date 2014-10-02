b039016eec78626f8c2bae79367ca9c1cc3463dd
macwilly <mtwillard29@gmail.com>
2014-10-01 23:31:46 -0400

    Added new javaScript
    Added the new javaScript page that will be used to make the dynamic
    selects.
    Added div to index for the selects

---
+++ dynamicSelect.js
@@ -0,0 +1,25 @@
+/* Mackenzie Willard
+* this is the script that is used to make the 
+* dynamic select menue for the first project in
+* client side programming*/
+
+/*This will get the element of a certian id as well as return it 
+*       document.getElementByI('select').action();
+*/
+function $(id){
+	return document.getElementById(id);
+}
+
+/*This will get the element using tag name and return it 
+*  doncument.getElementsByTagName('select')
+*/
+function $$(name){
+		//document.getElementsByTagName('body')[0];
+		return document.getElementsByTagName(name);
+	}
+/*
+*
+*/
+function makeSelects(which){
+	var select;
+}
