f0c40c39207a19ff2717744a3abaee47e55d99bf
macwilly <mtwillard29@gmail.com>
2014-10-05 14:04:33 -0400

    add photo
    added the photo names to the data

--- data.js
+++ data.js
@@ -8,22 +8,22 @@ data['doYouGo'] = ['Do you go to the gym','Yes I go to the gym','No I do not go 
 
 //the options from do you go to the gym
 data['Yes I go to the gym'] = ['What do you do at the gym','Classes','Cardio','Weight Training'];
-data['No I do not go to the gym'] = ['dyel','.jpg'];
+data['No I do not go to the gym'] = ['dyel','noDoNotLift.jpg'];
 
 //the options from what do you do at the gym
-data['Classes'] = ['dyel','.jpg'];
-data['Cardio'] = ['dyel','.jpg'];
+data['Classes'] = ['dyel','takeClasses.jpg'];
+data['Cardio'] = ['dyel','cardio.jpg'];
 data['Weight Training'] = ['What do you do for weight training', 'Machines','TRX','Body Weight','Free Weights'];
 
 // the options from what do you do for weight trainging
-data['Machines'] = ['dyel','.jpg'];
-data['TRX'] = ['dyel','.jpg'];
-data['Body Weight'] = ['dyel','.jpg'];
+data['Machines'] = ['dyel','machineLift.jpg'];
+data['TRX'] = ['dyel','trx.jpg'];
+data['Body Weight'] = ['dyel','kittyYoga.jpg'];
 data['Free Weights'] = ['What are you doing with the free weights', 'Body Building','Crossfit'];
 
 //the options from what are you doing with the free weights
 data['Body Building'] = ['What is your body building like','I look like I body build','I am strong', 'I compete'];
-data['Crossfit'] = ['dyel','.jpg'];
+data['Crossfit'] = ['dyel','crossfit.jpg'];
 
 // the options from what is your body building like 
 data['I look like I body build'] = ['Are you aesthetic','Yes I am aesthetic','I am Big','No I am not aesthetic'];
@@ -32,29 +32,29 @@ data['I compete'] = ['Do you actually compete','Yes of course I compete','No I j
 
 //the options from are you aesthetic
 data['Yes I am aesthetic'] = ["Are people'Mirin","Of course people are 'Mirin","Yeah not really sure what 'Mirin is"];
-data['I am Big'] = ['dyel','.jpg'];
-data['No I am not aesthetic'] = ['dyel','.jpg'];
+data['I am Big'] = ['dyel','spongebobBig.jpg'];
+data['No I am not aesthetic'] = ['dyel','notAesthetic.jpg'];
 
 //the data from are you really strong
 data['Yes I am really strong'] = ['Can you do 3 plates for 15 reps','Yes I can','No that is too much for me'];
-data['Well I guess not that strong'] = ['dyel','.jpg'];
+data['Well I guess not that strong'] = ['dyel','noImNotStrong.jpg'];
 
 // the options for do you actutally compete
 data['Yes of course I compete'] = ['Did you win','Yes I won','No I did not'];
-data['No I just said I did to see the options here'] = ['dyel','.jpg'];
+data['No I just said I did to see the options here'] = ['dyel','noCompetition.jpg'];
 
 //the options from are people 'Mirin
-data["Of course people are 'Mirin"] = ['dyel','.jpg'];
-data["Yeah not really sure what 'Mirin is"] = ['dyel','.jpg'];
+data["Of course people are 'Mirin"] = ['dyel','notMirin.jpg'];
+data["Yeah not really sure what 'Mirin is"] = ['dyel','whatIsMirin.jpg'];
 
 // the options from Can you do 3 plates for 15 reps
-data['Yes I can'] =  ['dyel','.jpg'];
-data['No that is too much for me'] =  ['dyel','.jpg'];
+data['Yes I can'] =  ['dyel','batmanLift.jpg'];
+data['No that is too much for me'] =  ['dyel','ronBench.jpg'];
 
 //the options from Did you win
 data['Yes I won'] = ['Are you Arnold','Yes I am Arnold','No I am not'];
-data['No I did not'] =  ['dyel','.jpg'];
+data['No I did not'] =  ['dyel','didNotWin.jpg'];
 
-//the options from are you aronold
-data['Yes I am Arnold'] = ['dyel','.jpg'];
-data['No I am not'] = ['dyel','.jpg'];
+//the options from are you arnold
+data['Yes I am Arnold'] = ['dyel','arnold.jpg'];
+data['No I am not'] = ['dyel','notArnold.jpg'];
